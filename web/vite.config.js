f6173670f2615cc8e28b74a300e8b78ecfcfa50c
dayonggo123 <dayonggo@gmail.com>
2026-04-22 23:51:11 +0800

    chore: add Veo channel to frontend channel constants and config

--- web/vite.config.js
+++ web/vite.config.js
@@ -19,16 +19,15 @@ For commercial licensing, please contact support@quantumnous.com
 
 import react from '@vitejs/plugin-react';
 import { defineConfig, transformWithEsbuild } from 'vite';
-import pkg from '@douyinfe/vite-plugin-semi';
 import path from 'path';
 import { codeInspectorPlugin } from 'code-inspector-plugin';
-const { vitePluginSemi } = pkg;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@douyinfe/semi-ui/dist/css/semi.css': path.resolve(__dirname, './node_modules/@douyinfe/semi-ui/dist/css/semi.css'),
     },
   },
   plugins: [
@@ -51,9 +50,6 @@ export default defineConfig({
       },
     },
     react(),
-    vitePluginSemi({
-      cssLayer: true,
-    }),
   ],
   optimizeDeps: {
     force: true,
