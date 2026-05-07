9f37c5b959fa57440ea1e59934ac8212e80c85c9
dayonggo123 <dayonggo@gmail.com>
2026-05-07 11:31:16 +0800

    debug(sitemap): add X-Debug-Sitemap header to verify handler invocation

--- controller/misc.go
+++ controller/misc.go
@@ -375,7 +375,7 @@ func GetSitemap(c *gin.Context) {
 
 	c.Header("Content-Type", "application/xml; charset=utf-8")
 	c.Header("Cache-Control", "public, max-age=3600")
-	c.Header("Content-Encoding", "identity")
+	c.Header("X-Debug-Sitemap", "handler-v2")
 
 	urls := []string{
 		serverAddr + "/",
