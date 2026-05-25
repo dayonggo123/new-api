7511f35fb49f8c8ef6e654b5aea1574a316f8cc7
dayonggo123 <dayonggo@gmail.com>
2026-05-25 18:58:59 +0800

    feat: add Seedream 5.0-lite/4.5 and Seedance 2.0 model aliases to VolcEngine/DoubaoVideo channels

--- relay/channel/task/doubao/constants.go
+++ relay/channel/task/doubao/constants.go
@@ -6,7 +6,9 @@ var ModelList = []string{
 	"doubao-seedance-1-0-lite-i2v",
 	"doubao-seedance-1-5-pro-251215",
 	"doubao-seedance-2-0-260128",
+	"seedance-2-0-260128",
 	"doubao-seedance-2-0-fast-260128",
+	"seedance-2-0-fast-260128",
 }
 
 var ChannelName = "doubao-video"
@@ -16,7 +18,9 @@ var ChannelName = "doubao-video"
 // 系统在检测到视频输入时自动乘以此折扣。
 var videoInputRatioMap = map[string]float64{
 	"doubao-seedance-2-0-260128":      28.0 / 46.0, // ~0.6087
+	"seedance-2-0-260128":             28.0 / 46.0,
 	"doubao-seedance-2-0-fast-260128": 22.0 / 37.0, // ~0.5946
+	"seedance-2-0-fast-260128":        22.0 / 37.0,
 }
 
 func GetVideoInputRatio(modelName string) (float64, bool) {
--- relay/channel/volcengine/constants.go
+++ relay/channel/volcengine/constants.go
@@ -8,10 +8,19 @@ var ModelList = []string{
 	"Doubao-lite-32k",
 	"Doubao-lite-4k",
 	"Doubao-embedding",
+	// Seedream 图片生成模型
+	"Doubao-Seedream-5.0-lite",
+	"Doubao-Seedream-4.5",
 	"doubao-seedream-4-0-250828",
 	"seedream-4-0-250828",
+	// Seedance 视频生成模型（通过 VolcEngine 通道也可调用）
 	"doubao-seedance-1-0-pro-250528",
 	"seedance-1-0-pro-250528",
+	"doubao-seedance-2-0-260128",
+	"seedance-2-0-260128",
+	"doubao-seedance-2-0-fast-260128",
+	"seedance-2-0-fast-260128",
+	// 文本模型
 	"doubao-seed-1-6-thinking-250715",
 	"seed-1-6-thinking-250715",
 }
