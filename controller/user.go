cabb8c062e45ec230d9b9fcda9feb52f8880ef74
dayonggo123 <dayonggo@gmail.com>
2026-06-01 10:37:57 +0800

    feat: expose access_token in /api/user/self response

--- controller/user.go
+++ controller/user.go
@@ -460,8 +460,9 @@ func GetSelf(c *gin.Context) {
 		"linux_do_id":       user.LinuxDOId,
 		"setting":           user.Setting,
 		"stripe_customer":   user.StripeCustomer,
-		"sidebar_modules":   userSetting.SidebarModules, // 正确提取sidebar_modules字段
-		"permissions":       permissions,                // 新增权限字段
+		"sidebar_modules":   userSetting.SidebarModules,
+		"permissions":       permissions,
+		"access_token":      user.AccessToken,
 	}
 
 	c.JSON(http.StatusOK, gin.H{
