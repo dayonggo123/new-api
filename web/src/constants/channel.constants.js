aace94d8069f0bec470bb8edd43926f76ae7f3d5
dayonggo123 <dayonggo@gmail.com>
2026-07-30 11:05:00 +0800

    feat(web): add Muyu channel type to frontend
    - Add Muyu (type 69) to channel.constants.js
    - Color: blue, matching other video generation channels

--- web/src/constants/channel.constants.js
+++ web/src/constants/channel.constants.js
@@ -244,6 +244,11 @@ export const CHANNEL_OPTIONS = [
     color: 'green',
     label: 'HongniaoAI',
   },
+  {
+    value: 69,
+    color: 'blue',
+    label: 'Muyu',
+  },
 ];
 
 // Channel types that support upstream model list fetching in UI.
